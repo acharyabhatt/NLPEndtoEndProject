23a5a9c74895802f8238b1d716e37a9412d9e1d1
Arpan Bhattacharya <abarpanacharyabhatt@gmail.com>
2024-03-26 20:50:04 +0530

    data validation with modified files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from nlptextsummarize.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from nlptextsummarize.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from nlptextsummarize.logging import logger
 
 
@@ -12,3 +13,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+     
+     
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
